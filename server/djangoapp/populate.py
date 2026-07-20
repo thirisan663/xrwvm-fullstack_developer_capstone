32e1819c6bc51065321df029ed3b7a6bb216a3d7
thirisan663 <thirisan662003@gmail.com>
2026-07-20 10:02:46 -0400

    Fix populate.py indentation

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -1,25 +1,27 @@
 from .models import CarMake, CarModel
 
+
 def initiate():
     car_make_data = [
-        {"name":"NISSAN", "description":"Great cars. Japanese technology"},
-        {"name":"Mercedes", "description":"Great cars. German technology"},
-        {"name":"Audi", "description":"Great cars. German technology"},
-        {"name":"Kia", "description":"Great cars. Korean technology"},
-        {"name":"Toyota", "description":"Great cars. Japanese technology"},
+        {"name": "NISSAN", "description": "Great cars. Japanese technology"},
+        {"name": "Mercedes", "description": "Great cars. German technology"},
+        {"name": "Audi", "description": "Great cars. German technology"},
+        {"name": "Kia", "description": "Great cars. Korean technology"},
+        {"name": "Toyota", "description": "Great cars. Japanese technology"},
     ]
 
     car_make_instances = []
+
     for data in car_make_data:
         car_make_instances.append(
             CarMake.objects.create(
-                name=data['name'],
-                description=data['description']
+                name=data["name"],
+                description=data["description"]
             )
         )
 
     # Create CarModel instances with the corresponding CarMake instances
-        car_model_data = [
+    car_model_data = [
         {"name": "Pathfinder", "type": "SUV", "year": 2023, "car_make": car_make_instances[0]},
         {"name": "Qashqai", "type": "SUV", "year": 2023, "car_make": car_make_instances[0]},
         {"name": "XTRAIL", "type": "SUV", "year": 2023, "car_make": car_make_instances[0]},
@@ -39,8 +41,8 @@ def initiate():
 
     for data in car_model_data:
         CarModel.objects.create(
-            name=data['name'],
-            car_make=data['car_make'],
-            type=data['type'],
-            year=data['year']
+            name=data["name"],
+            car_make=data["car_make"],
+            type=data["type"],
+            year=data["year"]
         )
